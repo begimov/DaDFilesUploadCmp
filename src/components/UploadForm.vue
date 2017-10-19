cbd911bebc8ecaec9a356409e7c0d220bfa55813
begimov <begimov@gmail.com>
2017-10-19 06:30:37 +0300

    files selection form built and styled

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,5 +1,12 @@
 <template>
-  <p>Upload Form</p>
+  <div class="dnd">
+      <input type="file" name="files[]" id="file" class="dnd__input" multiple>
+      <label for="file" class="dnd__label">
+          <strong>
+              Перенесите сюда файлы или кликните, чтобы их выбрать 
+          </strong>
+      </label>
+  </div>
 </template>
 
 <script>
@@ -7,3 +14,23 @@ export default {
   //
 };
 </script>
+
+<style scoped>
+    .dnd {
+        --dnd-min-height: 200px;
+        width: 100%;
+        min-height: var(--dnd-min-height);
+        background-color: #F9F9F9;
+        position: relative;
+        border: 2px dashed #DDD;
+    }
+    .dnd__input {
+        display: none;
+    }
+    .dnd__label {
+        display: block;
+        text-align: center;
+        margin: calc(var(--dnd-min-height) / 2) 20px;
+    }
+</style>
+
