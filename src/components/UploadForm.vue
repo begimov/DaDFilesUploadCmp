b798a690bd5ea7feb9bd1012b16320cc8377835c
begimov <begimov@gmail.com>
2017-10-19 14:29:44 +0300

    method to generate file object and push it to data prop implemented

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -42,25 +42,49 @@ export default {
     },
     addFiles(files) {
       Array.from(files).forEach(function(file) {
-          this.storeMeta(file).then((fileObject) => {
-              // upload
-          }, (fileObject) => {
-              // failed
-          });
+        this.storeMeta(file).then(
+          fileObject => {
+            // upload
+          },
+          fileObject => {
+            // failed
+          }
+        );
       });
     },
     storeMeta(file) {
-        // create file object
-        return new Promise((resolve, reject) => {
-            this.$http.post('http://localhost:8080/vueupload/store.php', {
-                name: file.name
-            }).then((res) => {
-                // set id on file object
-                // resolve
-            }, () => {
-                // reject
-            });
-        });
+      // create file object
+      return new Promise((resolve, reject) => {
+        this.$http
+          .post("http://localhost:8080/vueupload/store.php", {
+            name: file.name
+          })
+          .then(
+            res => {
+              // set id on file object
+              // resolve
+            },
+            () => {
+              // reject
+            }
+          );
+      });
+    },
+    generateFileObj(file) {
+      const fileObjIndex = this.files.push({
+        id: null,
+        file: file,
+        progress: 0,
+        failed: false,
+        loadedBytes: 0,
+        totalBytes: 0,
+        timeStarted: new Date().getTime(),
+        secondsRemaining: 0,
+        finished: false,
+        cancelled: false,
+        xhr: null
+      }) - 1;
+      return this.files[fileObjIndex];
     }
   }
 };
